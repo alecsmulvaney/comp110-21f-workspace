005a851c0f93b496190585d1015d22f6d5b9adb6
Alec Mulvaney <alecsmulvaney@unc.edu>
2021-09-15 00:47:49 -0400

    this is hard

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -1,3 +1,32 @@
 """Finding duplicate letters in a word."""
 
-__author__ = "123456789"
+__author__ = "730392059"
+
+
+word: str = (input("Enter a word: "))
+count: int = len(word)
+last = word[count - 1]
+i = 0
+x = 1
+letter = word[i]
+a = word[x]
+
+
+while count > 0:
+    while a != last: 
+        if letter == a:
+            t = str("True") 
+        x = x + 1
+    count = count - 1
+    i = i + 1
+    x = i + 1
+
+    
+
+if t == "True":
+    print("True")
+else:
+    print("False")
+
+
+print(a)
--- exercises/ex03/happy_trees.py
+++ exercises/ex03/happy_trees.py
@@ -1,6 +1,13 @@
 """Drawing forests in a loop."""
 
-__author__ = "123456789"
+__author__ = "730392059"
 
 # The string constant for the pine tree emoji
 TREE: str = '\U0001F332'
+
+depth: int = int(input("How deep do you want? "))
+print("Depth:", depth)
+depthtwo: int = depth
+while depth > 0:
+    print((TREE) * (depthtwo - depth + 1))
+    depth = depth - 1
