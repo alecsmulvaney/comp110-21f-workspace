82b6e0d55e67c41da257243ea7d82aaf72bf4e90
Alec Mulvaney <alecsmulvaney@unc.edu>
2021-09-22 22:28:11 -0400

    done? cyoa

--- exercises/ex03/find_duplicates.py
+++ exercises/ex03/find_duplicates.py
@@ -5,19 +5,20 @@ __author__ = "730392059"
 
 word: str = (input("Enter a word: "))
 i: int = 0
-x: int = 1
+x: int = 0
 letter: str = word[i]
 a: str = word[x]
 var: bool = False
 
 while i < len(word):
     letter = word[i]
-    while x < len(word): 
+    x = i + 1
+    while x < len(word):
+        a = word[x] 
         if letter == a:
             var = True
-        a = word[x]
         x = x + 1
-    
     i = i + 1
     
+
 print("Found duplicate: " + str(var))
