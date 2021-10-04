7dd018c80f66adf18ceeb2f2c72d621449284e78
Alec Mulvaney <alecsmulvaney@unc.edu>
2021-10-03 20:39:16 -0400

    done ex05

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,47 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730392059"
 
 # Define your functions below
+
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Function to return only evans."""
+    i: int = 0
+    while i < len(xs):
+        if xs[i] % 2 != 0:
+            xs.pop(i)
+        i += 1
+    return xs
+
+
+def sub(xs: list[int], x: int, y: int) -> list[int]:
+    """Function to return index items."""
+    ys: list[int] = []
+    i: int = x
+    if xs == []:
+        return []
+    if x >= len(xs):
+        return []
+    if x < 0:
+        i = 0
+    if y > len(xs):
+        y = len(xs)
+    while i <= y - 1:
+        ys.append(xs[i])
+        i += 1
+    return ys
+
+
+def concat(xs: list[int], ys: list[int]) -> list[int]:
+    """Function that adds two lists together."""
+    cs: list[int] = []
+    i: int = 0
+    while i < len(xs):
+        cs.append(xs[i])
+        i += 1
+    i = 0
+    while i < len(ys):
+        cs.append(ys[i])   
+        i += 1
+    return cs
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,98 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730392059"
+
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub
+from exercises.ex05.utils import concat
+
+
+def test_only_evens() -> None:
+    """Testing only evens."""
+    xs: list[int] = [2, 4, 5, 6]
+    ys: list[int] = [3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+    assert only_evens(xs) == [2, 4, 4]
+    assert only_evens(ys) == [4, 4, 4]
+
+
+def test_only_evens_two() -> None:
+    """Testing only evens."""
+    xs: list[int] = [2, 4, 5, 6]
+    ys: list[int] = [3, 4, 5, 6]
+    assert only_evens(xs) == [2, 4, 6]
+    assert only_evens(xs) == [2, 5, 4]
+    assert only_evens(ys) == [4, 3, 4]
+
+
+def test_only_evens_three() -> None:
+    """Testing only evens."""
+    xs: list[int] = [2, 4, 5, 6]
+    ys: list[int] = [3, 4, 5, 6]
+    assert only_evens(xs) == [2, 5, 6]
+    assert only_evens(xs) == [2, 7, 4]
+    assert only_evens(ys) == [4, 8, 4]
+
+
+def test_sub() -> None:
+    """Testing sub function."""
+    xs: list[int] = [2, 4, 5, 6]
+    x: int = 1
+    y: int = 3
+    z: int = 4
+    assert sub(xs, x, y) == [2, 4, 6]
+    assert sub(xs, x, y) == [2, 4, 5]
+    assert sub(xs, x, z) == [2, 4, 5, 6]
+
+
+def test_sub_two() -> None:
+    """Testing sub function."""
+    xs: list[int] = [2, 4, 6, 6]
+    x: int = 1
+    y: int = 3
+    z: int = 4
+    assert sub(xs, x, y) == [2, 4, 6]
+    assert sub(xs, x, y) == [2, 3, 5]
+    assert sub(xs, x, z) == [2, 4, 5, 6]
+
+
+def test_sub_three() -> None:
+    """Testing sub function."""
+    xs: list[int] = [2, 4, 5, 3]
+    x: int = 2
+    y: int = 3
+    z: int = 4
+    assert sub(xs, x, y) == [2, 4, 6]
+    assert sub(xs, x, y) == [2, 4, 5]
+    assert sub(xs, x, z) == [2, 4, 5, 6]
+
+
+def test_concat() -> None:
+    """Testing concat function."""
+    xs: list[int] = [2, 4, 5, 6]
+    ys: list[int] = [3, 4, 5, 6]
+    zs: list[int] = [2, 4, 5, 5]
+    assert concat(xs, ys) == [2, 4, 5, 6, 3, 4, 5, 6]
+    assert concat(xs, ys) == [2, 4, 4]
+    assert concat(ys, zs) == [4, 4, 4]
+
+
+def test_concat_two() -> None:
+    """Testing concat function."""
+    xs: list[int] = [2, 4, 5, 6]
+    ys: list[int] = [3, 4, 5, 6]
+    zs: list[int] = [2, 4, 5, 5]
+    assert concat(xs, ys) == [2, 3, 5, 6, 3, 4, 5, 6]
+    assert concat(xs, ys) == [2, 5, 4]
+    assert concat(ys, zs) == [4, 5, 4]
+
+
+def test_concat_three() -> None:
+    """Testing concat function."""
+    xs: list[int] = [2, 4, 6, 6]
+    ys: list[int] = [3, 4, 6, 6]
+    zs: list[int] = [2, 4, 6, 5]
+    assert concat(xs, ys) == [2, 3, 5, 6, 3, 4, 5, 6]
+    assert concat(xs, ys) == [2, 5, 4]
+    assert concat(ys, zs) == [4, 5, 4]
